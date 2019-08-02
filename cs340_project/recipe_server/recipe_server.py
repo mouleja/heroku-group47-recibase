a0fceced9e3a0035f0baaa80160364ce3dfd7667
JasonM <mouelja@oregonstate.edu>
2019-08-01 20:03:13 -0700

    More refinement to User page including complete add, edit, and delete functionality.

--- cs340_project/recipe_server/recipe_server.py
+++ cs340_project/recipe_server/recipe_server.py
@@ -7,8 +7,9 @@
 #   using mariaDB database running on remote server
 #
 # Code adapted from course tutorial at: 
-# https://github.com/knightsamar/CS340_starter_flask_app/
-# and https://www.roytuts.com/python-web-application-crud-example-using-flask-and-mysql/
+# https://github.com/knightsamar/CS340_starter_flask_app/ and
+# https://www.roytuts.com/python-web-application-crud-example-using-flask-and-mysql/
+# https://pynative.com/python-mysql-tutorial/
 #########################################################################
 
 import pymysql, datetime
@@ -23,7 +24,10 @@ def get_recipes():
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(
-            'SELECT * FROM Recipe;'
+            '''SELECT r.recipeId, r.name recipeName, r.date, u.name username, s.name sourceName 
+            FROM Recipe r 
+            INNER JOIN User u on u.userId = r.userId
+            INNER JOIN Source s on s.sourceId = r.sourceId'''
         )
         result = cursor.fetchall() or {'text':'Nothing in recipe!!'}
         print("Getting all recipes.")
@@ -125,7 +129,7 @@ def delete_recipe(id):
         recipe = cursor.fetchone() or {'rname':'Error'}
 
         print("Delete recipe?", recipe)
-        return render_template('confirm_delete.html', recipe=recipe, recipeId=id)
+        return render_template('confirm_delete.html', recipe=recipe, recipeId=id, deleteType='recipe')
     except Exception as e:
         print("Error getting recipe",id, e)
     finally:
@@ -136,9 +140,39 @@ def delete_recipe(id):
 
 @app.route('/delete', methods=['POST'])
 def delete():
-    id = request.form['id']
-    print("Delete processed for recipe id #",id)
-    return redirect("/get_recipes")
+    userId = request.form['userId']
+    password = request.form['password']
+    deleteType = request.form['deleteType']
+    recipeId = request.form.get('recipeId')
+    username = request.form.get('username')
+    print("Deleting:", deleteType, userId, recipeId, username)
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM User WHERE userId = %s and password = %s', (userId, password))
+        checkpw = cursor.fetchone() # True if password wrong
+        if not checkpw:
+            return render_template('confirm_delete.html', 
+                    userId=userId, deleteType=deleteType, recipeId=recipeId, username=username, pwError = True)
+
+        if deleteType == 'recipe':
+            print("Delete processed for recipe id #", recipeId)
+            return redirect("/get_recipes")
+
+        if deleteType == 'user':
+            cursor.execute('DELETE FROM User WHERE userId = %s', (userId,))
+            conn.commit()
+            print("Deleted user", userId, "from the database.")
+            return redirect('/')
+
+    except Exception as e:
+        print("Error deleting", userId, recipeId, deleteType, e)
+    finally:
+        cursor.close() 
+        conn.close()
+
+    return 'Something went wrong in delete!'
+
 
 @app.route('/user_entry')
 def user_entry():
@@ -193,13 +227,13 @@ def user_page():
     try:
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT userId, name FROM User WHERE name = %s and password = %s', (username, password))
+        cursor.execute('SELECT * FROM User WHERE name = %s and password = %s', (username, password))
         user = cursor.fetchone()
 
         if not user:
             return render_template('user_entry.html', error=True)
 
-        cursor.execute('SELECT recipeId, name, date FROM Recipe WHERE userId = %s', (user['userId'],))
+        cursor.execute('SELECT * FROM Recipe WHERE userId = %s', (user['userId'],))
         recipes = cursor.fetchall()
 
         return render_template('user_page.html', user=user, recipes=recipes)
@@ -211,6 +245,68 @@ def user_page():
         conn.close()
 
     return 'Something went wrong in user_page!'
+
+@app.route('/change_username', methods=['POST'])
+def change_username():
+    userId = request.form['userId']
+    newName = request.form['newName']
+    user = {'userId': userId, 'name': newName, 'password': request.form['password']}
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('SELECT * FROM User WHERE name = %s', (newName,))
+        nameUsed = cursor.fetchone()
+        nameFree = not nameUsed         # python trick: nameFree = True if nameUsed is empty else False
+        if nameFree:
+            cursor.execute('UPDATE User SET name = %s WHERE userId = %s', (newName, userId))
+            conn.commit()
+            print("Successfully updated username for", userId, "to", newName)
+        else:
+            user['name'] = request.form['username']
+            print("Failed update username for", userId, "to", newName)
+
+        cursor.execute('SELECT * FROM Recipe WHERE userId = %s', (userId,))
+        recipes = cursor.fetchall()
+        return render_template('user_page.html', user=user, recipes=recipes, usedError=not nameFree)
+    
+    except Exception as e:
+        print("Error changing username", userId, e)
+    finally:
+        cursor.close() 
+        conn.close()
+
+    return 'Something went wrong in change_username!'
+
+@app.route('/change_password', methods=['POST'])
+def change_password():
+    userId = request.form['userId']
+    newPass = request.form['newPass']
+    user = {'userId': userId, 'name': request.form['username'], 'password': newPass}
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute('UPDATE User SET password = %s WHERE userId = %s', (newPass, userId))
+        conn.commit()
+        print("Successfully updated password for", userId)
+
+        cursor.execute('SELECT * FROM Recipe WHERE userId = %s', (userId,))
+        recipes = cursor.fetchall()
+        return render_template('user_page.html', user=user, recipes=recipes)
+    
+    except Exception as e:
+        print("Error changing password", userId, e)
+    finally:
+        cursor.close() 
+        conn.close()
+
+    return 'Something went wrong in change_password!'
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    userId = request.form['userId']
+    username = request.form['username']
+    return render_template('confirm_delete.html', userId=userId, username=username, deleteType='user')
+
 
 @app.route('/')
 def index():
